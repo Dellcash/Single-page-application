c2de31dc1782a1cc9506b000c26214fa02c62d79
Dellcash <77158396+Dellcash@users.noreply.github.com>
2021-08-15 05:59:48 +0430

    Add files via upload

---
+++ src/components/users/CardView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card d mt-2" >
+    <div class="card-header ">
+      <router-link class="user" :to="{ name: 'userId', params: { id: user.id } }">
+        {{ user.name }}
+      </router-link>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Username: {{ user.username }}</li>
+      <li class="list-group-item">Email: {{ user.email }}</li>
+      <li class="list-group-item">Website: {{ user.website }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: Object,
+  },
+};
+</script>
+
+<style scoped>
+/* .d{
+  direction: ltr;
+} */
+.router-link-active {
+  color: rgba(0, 0, 0, 0.9) !important;
+  text-decoration: none;
+  font-weight: bold;
+  cursor: auto;
+}
+.user {
+  text-decoration: none;
+  transition: all 0.3s;
+}
+*{
+  direction: ltr;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router.js'
+import "animate.css"
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
