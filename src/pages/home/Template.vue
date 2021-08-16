228ba0df9ee678276312002e4526664f8ae262fe
Dellcash <77158396+Dellcash@users.noreply.github.com>
2021-08-16 07:13:26 +0430

    Update Template.vue

--- src/pages/home/Template.vue
+++ src/pages/home/Template.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <section-1 />
+    <section-2 />
   </div>
 </template>
 
 <script>
 import section1 from "./section-1.vue";
+import section2 from "./section-2.vue";
+
 export default {
-  components: { section1 },
+  components: { section1, section2 },
   setup() {
     return {};
   },
